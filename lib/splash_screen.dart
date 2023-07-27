4ae6682ecf3035b9a869844193ca5dff2cbf49d9
ebinanony <ebinantony0088@gmail.com>
2023-07-27 21:48:22 +0530

    splash`

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,21 +1,65 @@
 import 'package:flutter/material.dart';
 
-
-void main() {
-  // attach widget tree to the ui
-  runApp(const MaterialApp(
-    home: SplashScreen(),
-  ));
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
 }
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1500),
+    );
+
+    _controller.forward();
+
+    // Uncomment the line below to navigate to the next screen after the splash screen animation finishes.
+    // _controller.addStatusListener((status) {
+    //   if (status == AnimationStatus.completed) {
+    //     Navigator.of(context).pushReplacement(
+    //       MaterialPageRoute(
+    //         builder: (_) => NextScreen(), // Replace NextScreen with your desired screen.
+    //       ),
+    //     );
+    //   }
+    // });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Splash Screen"),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlutterLogo(
+                  size: _controller.value * 100,
+                ),
+                SizedBox(height: 16),
+                Text(
+                  '${(_controller.value * 100).toInt()}%',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
