5956c3b887192a4dcbaccfdf5a7d01d8aa156a86
ebinanony <ebinantony0088@gmail.com>
2023-08-01 21:44:44 +0530

    insta splash

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,64 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SplashScreen extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(milliseconds: 1500),
-    );
-
-    _controller.forward();
-
-    // Uncomment the line below to navigate to the next screen after the splash screen animation finishes.
-    // _controller.addStatusListener((status) {
-    //   if (status == AnimationStatus.completed) {
-    //     Navigator.of(context).pushReplacement(
-    //       MaterialPageRoute(
-    //         builder: (_) => NextScreen(), // Replace NextScreen with your desired screen.
-    //       ),
-    //     );
-    //   }
-    // });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Center(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, child) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FlutterLogo(
-                  size: _controller.value * 100,
-                ),
-                SizedBox(height: 16),
-                Text(
-                  '${(_controller.value * 100).toInt()}%',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ],
-            );
-          },
+        child: ShaderMask(
+          blendMode: BlendMode.srcIn,
+          shaderCallback: (Rect bounds) => LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.purple,
+              Colors.pink,
+              Colors.orange,
+            ],
+          ).createShader(bounds),
+          child: FaIcon(
+            FontAwesomeIcons.instagram,
+            size: 100,
+          ),
         ),
       ),
     );
