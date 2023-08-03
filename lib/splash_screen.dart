04e14ab330e67b3eb4fb8ccda346bdf0fccc3485
Ebin Antony <133174341+ebinanony@users.noreply.github.com>
2023-08-03 17:44:02 +0530

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:practice/login_screen.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({Key? key}) : super(key: key);
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToNextScreen();
+  }
+
+  void navigateToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 5));
+
+    // Replace the below line with the code to navigate to your desired screen.
+    // For example, you can use Navigator.pushReplacement() to navigate to the main screen.
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
-        child: ShaderMask(
-          blendMode: BlendMode.srcIn,
-          shaderCallback: (Rect bounds) => LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomRight,
-            colors: [
-              Colors.purple,
-              Colors.pink,
-              Colors.orange,
-            ],
-          ).createShader(bounds),
-          child: FaIcon(
-            FontAwesomeIcons.instagram,
-            size: 100,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              fit: BoxFit.fill,
+              image: AssetImage("assets/images/instasplash.gif"),
+            ),
+            SizedBox(height: 20),
+          ],
         ),
       ),
     );
